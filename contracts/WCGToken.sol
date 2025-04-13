ebb8b06c4c24ecec54f95ab1318c1774cad48f69
WishCryptoGrants-WCG <support@wishcryptogrants.com>
2025-04-13 12:21:41 +0530

    Update WCGToken.sol

--- contracts/WCGToken.sol
+++ contracts/WCGToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WCGToken is ERC20, Ownable {
+    constructor() ERC20("WishCryptoGrants", "WCG") Ownable(msg.sender) {
+        _mint(msg.sender, 12000000 * 10 ** decimals());
+    }
+}
